4c90060f860dde62a2cfdd5eb908c7b625369669
Leonardo Rodrigues <leonardorodrigues_f@hotmail.com>
2020-03-06 00:21:46 -0300

    Obtendo usuário por ID

--- src/main/java/com/leorfk/workshopmongo/services/UserService.java
+++ src/main/java/com/leorfk/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.leorfk.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.leorfk.workshopmongo.domain.User;
 import com.leorfk.workshopmongo.repository.UserRepository;
+import com.leorfk.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,11 @@ public class UserService {
 
 	public List<User> findAll(){
 		return userRepo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj = userRepo.findById(id);
+		// retorna um objeto ou lança uma exception
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
