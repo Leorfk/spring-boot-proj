3f88de8d9bf51f62897498a50379f59410403b41
leonardo rodrigues ferreira <leonardorodrigues_f@hotmail.com>
2020-03-22 22:46:32 -0300

    referenciando os posts do usuario

--- src/main/java/com/leorfk/workshopmongo/domain/User.java
+++ src/main/java/com/leorfk/workshopmongo/domain/User.java
@@ -1,8 +1,11 @@
 package com.leorfk.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="user")// caso não seja atribuido o nome da coleção será usado o nome da classe como referência
@@ -14,6 +17,10 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+	// DBRef serve para indicar ue está fazendo referencia a outra "tabela"
+	//lazy = true serve para não carregar a lista de posts assim que carregar um user
+	@DBRef(lazy = true)
+	private List<Post> posts = new ArrayList<>();
 	
 	public User() {}
 
@@ -71,5 +78,13 @@ public class User implements Serializable {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
 	}
 }
